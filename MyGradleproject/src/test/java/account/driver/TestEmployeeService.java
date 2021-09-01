4c2453f91121cd60967dfec6ee60cd20e300f689
Ashmitha <ashmithashree06@gmail.com>
2021-09-01 15:15:18 +0530

    first commit

--- MyGradleproject/src/test/java/account/driver/TestEmployeeService.java
+++ MyGradleproject/src/test/java/account/driver/TestEmployeeService.java
@@ -31,42 +31,42 @@ private static Employee emp2;
 	static void setUpBeforeClass() throws Exception {
 		mockEmployeeDao = Mockito.mock(EmployeeDao.class);
 		employeeService = new EmployeeServiceImpl(mockEmployeeDao);
-//		emp1=new Employee();
-//		emp1.setEmpNo(1);
-//		emp1.setEmpName("Hemant");
-//		emp1.setHireDate(LocalDate.of(2018,11,22));
-//		emp1.setSalary(8000);
-//		emp1.setJob("Developer");
-//		emp2=new Employee();
-//		emp2.setEmpNo(2);
-//		emp2.setEmpName("Varsha");
-//		emp2.setHireDate(LocalDate.of(2018,12,02));
-//		emp2.setSalary(9000);
-//		emp2.setJob("Tester");
-		Connection conn=null;
-		try {
-			conn= DBUtil.getDBConnection();
-			Statement stmt=conn.createStatement();
-			int count=stmt.executeUpdate("insert into public.emp(\r\n"
-					+ "	empno, ename, sal, deptno, job, \"Hiredate\", comm, mgr)\r\n"
-					+ "	VALUES (5, 'sachin', 12000, 2, 'sales', '2012-01-20', 2, 2)");
-			System.out.println("Records is inserted successfully");
-			}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}finally {
-			if(conn!=null)
-			{
-				try {
-					conn.close();
-				}
-				catch(SQLException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
+		emp1=new Employee();
+		emp1.setEmpNo(1);
+		emp1.setEmpName("Hemant");
+		emp1.setHireDate(LocalDate.of(2018,11,22));
+		emp1.setSalary(8000);
+		emp1.setJob("Developer");
+		emp2=new Employee();
+		emp2.setEmpNo(2);
+		emp2.setEmpName("Varsha");
+		emp2.setHireDate(LocalDate.of(2018,12,02));
+		emp2.setSalary(9000);
+		emp2.setJob("Tester");
+//		Connection conn=null;
+//		try {
+//			conn= DBUtil.getDBConnection();
+//			Statement stmt=conn.createStatement();
+//			int count=stmt.executeUpdate("insert into public.emp(\r\n"
+//					+ "	empno, ename, sal, deptno, job, \"Hiredate\", comm, mgr)\r\n"
+//					+ "	VALUES (5, 'sachin', 12000, 2, 'sales', '2012-01-20', 2, 2)");
+//			System.out.println("Records is inserted successfully");
+//			}
+//		catch(SQLException e)
+//		{
+//			e.printStackTrace();
+//		}finally {
+//			if(conn!=null)
+//			{
+//				try {
+//					conn.close();
+//				}
+//				catch(SQLException e)
+//				{
+//					e.printStackTrace();
+//				}
+//			}
+//		}
 		Mockito.when(mockEmployeeDao.getEmployees()).thenReturn(Arrays.asList(emp1,emp2));
 		
 	}
